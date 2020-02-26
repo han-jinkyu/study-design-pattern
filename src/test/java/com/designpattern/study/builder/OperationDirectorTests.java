82971687dd3c8cf0675927d3f6a22738b42a712a
Jinkyu Han <hjk1019@gmail.com>
2020-02-26 23:13:13 +0900

    Builder 패턴 코드에 대한 테스트 추가

---
+++ src/test/java/com/designpattern/study/builder/OperationDirectorTests.java
@@ -0,0 +1,79 @@
+package com.designpattern.study.builder;
+
+import com.designpattern.study.builder.builders.AddOperationBuilder;
+import com.designpattern.study.builder.builders.DivideOperationBuilder;
+import com.designpattern.study.builder.builders.MultiplyOperationBuilder;
+import com.designpattern.study.builder.builders.SubtractOperationBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OperationDirectorTests {
+
+    private OperationDirector director;
+    private AbstractOperationBuilder builder;
+    private int leftNumber, rightNumber;
+
+    @Before
+    public void setup() {
+        this.leftNumber = 100;
+        this.rightNumber = 20;
+    }
+
+    @Test
+    public void Should_BeAddOperator_When_UseAddOperationBuilder() {
+        // given
+        String given = String.format("%d + %d = %d", leftNumber, rightNumber, leftNumber + rightNumber);
+
+        // when
+        builder = new AddOperationBuilder(leftNumber, rightNumber);
+        director = new OperationDirector(builder);
+        String build = director.build();
+
+        // then
+        assertEquals(given, build);
+    }
+
+    @Test
+    public void Should_BeSubtractOperator_When_UseSubtractOperationBuilder() {
+        // given
+        String given = String.format("%d - %d = %d", leftNumber, rightNumber, leftNumber - rightNumber);
+
+        // when
+        builder = new SubtractOperationBuilder(leftNumber, rightNumber);
+        director = new OperationDirector(builder);
+        String build = director.build();
+
+        // then
+        assertEquals(given, build);
+    }
+
+    @Test
+    public void Should_BeMultiplyOperator_When_UseMultiplyOperationBuilder() {
+        // given
+        String given = String.format("%d * %d = %d", leftNumber, rightNumber, leftNumber * rightNumber);
+
+        // when
+        builder = new MultiplyOperationBuilder(leftNumber, rightNumber);
+        director = new OperationDirector(builder);
+        String build = director.build();
+
+        // then
+        assertEquals(given, build);
+    }
+
+    @Test
+    public void Should_BeDivideOperator_When_UseDivideOperationBuilder() {
+        // given
+        String given = String.format("%d / %d = %d", leftNumber, rightNumber, leftNumber / rightNumber);
+
+        // when
+        builder = new DivideOperationBuilder(leftNumber, rightNumber);
+        director = new OperationDirector(builder);
+        String build = director.build();
+
+        // then
+        assertEquals(given, build);
+    }
+}
