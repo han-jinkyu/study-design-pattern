375ac2c09e2d2d14052e5265b9a9a28ec28eb4b6
Jinkyu Han <hjk1019@gmail.com>
2020-03-10 23:44:21 +0900

    Flyweight 패턴 코드에 대한 테스트 추가

---
+++ src/test/java/com/designpattern/study/flyweight/ExpressionFlyweightFactoryTests.java
@@ -0,0 +1,88 @@
+package com.designpattern.study.flyweight;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExpressionFlyweightFactoryTests {
+
+    private ExpressionFlyweightFactory factory;
+    private int leftNumber, rightNumber;
+
+    @Before
+    public void setup() {
+        leftNumber = 100;
+        rightNumber = 20;
+        factory = ExpressionFlyweightFactory.getInstance();
+    }
+
+    @Test
+    public void should_BeSameExpressionFlyweight_When_SameOperand() {
+        // given
+        String operand = "+";
+        ExpressionFlyweight given = factory.getExpressionFlyweight(operand);
+
+        // when
+        ExpressionFlyweight result = factory.getExpressionFlyweight(operand);
+
+        // then
+        assertEquals(given, result);
+    }
+
+    @Test
+    public void should_BeAddExpression_When_AddOperand() {
+        // given
+        String operand = "+";
+        String given = String.format("%d%s%d", leftNumber, operand, rightNumber);
+
+        // when
+        ExpressionFlyweight flyweight = factory.getExpressionFlyweight(operand);
+        String result = flyweight.operate();
+
+        // then
+        assertEquals(given, result);
+    }
+
+    @Test
+    public void should_BeSubtractExpression_When_SubtractOperand() {
+        // given
+        String operand = "-";
+        String given = String.format("%d%s%d", leftNumber, operand, rightNumber);
+
+        // when
+        ExpressionFlyweight flyweight = factory.getExpressionFlyweight(operand);
+        String result = flyweight.operate();
+
+        // then
+        assertEquals(given, result);
+    }
+
+    @Test
+    public void should_BeMultiplyExpression_When_MultiplyOperand() {
+        // given
+        String operand = "*";
+        String given = String.format("%d%s%d", leftNumber, operand, rightNumber);
+
+        // when
+        ExpressionFlyweight flyweight = factory.getExpressionFlyweight(operand);
+        String result = flyweight.operate();
+
+        // then
+        assertEquals(given, result);
+    }
+
+    @Test
+    public void should_BeDivideExpression_When_DivideOperand() {
+        // given
+        String operand = "/";
+        String given = String.format("%d%s%d", leftNumber, operand, rightNumber);
+
+        // when
+        ExpressionFlyweight flyweight = factory.getExpressionFlyweight(operand);
+        String result = flyweight.operate();
+
+        // then
+        assertEquals(given, result);
+    }
+}
