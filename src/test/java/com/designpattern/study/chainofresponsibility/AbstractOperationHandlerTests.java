297ea104f0ab827d0bf63f0d272aeb18e7e35bc1
Jinkyu Han <hjk1019@gmail.com>
2020-03-12 23:40:04 +0900

    Chain of Responsibility 패턴 코드에 대한 테스트 추가

---
+++ src/test/java/com/designpattern/study/chainofresponsibility/AbstractOperationHandlerTests.java
@@ -0,0 +1,53 @@
+package com.designpattern.study.chainofresponsibility;
+
+import com.designpattern.study.chainofresponsibility.handlers.AddOperationHandler;
+import com.designpattern.study.chainofresponsibility.handlers.DivideOperationHandler;
+import com.designpattern.study.chainofresponsibility.handlers.MultiplyOperationHandler;
+import com.designpattern.study.chainofresponsibility.handlers.SubtractOperationHandler;
+import com.sun.tools.javac.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AbstractOperationHandlerTests {
+
+    private int leftNumber, rightNumber;
+
+    @Before
+    public void setup() {
+        leftNumber = 100;
+        rightNumber = 20;
+    }
+
+    @Test
+    public void should_BeSame_When_DoOperations() {
+        // given
+        AbstractOperationHandler handler = new AddOperationHandler("+");
+
+        handler.setNext(new SubtractOperationHandler("-"))
+            .setNext(new MultiplyOperationHandler("*"))
+            .setNext(new DivideOperationHandler("/"));
+
+        List<Integer> answers = List.of(
+            leftNumber + rightNumber,
+            leftNumber - rightNumber,
+            leftNumber * rightNumber,
+            leftNumber / rightNumber
+        );
+
+        // when
+        List<Request> requests = List.of("+", "-", "*", "/")
+            .map(op -> {
+                String expression = String.format("%d%s%d", leftNumber, op, rightNumber);
+                return new Request(expression);
+            });
+
+        // then
+        for (int idx = 0; idx < requests.length(); idx++) {
+            int answer = handler.handleRequest(requests.get(idx));
+            int given = answers.get(idx);
+            assertEquals(given, answer);
+        }
+    }
+}
